e838eeab3fbe9fc4fda2f3d84f90807401e6bb7b
Nakhun12310 <163088668+Nakhun12310@users.noreply.github.com>
2025-03-17 10:40:25 +0700

    Create GameList.lua

---
+++ GameList.lua
@@ -0,0 +1,16 @@
+local Games = {
+  [2753915549] = "nil", -- Blox Fruit Sea 1
+  [4442272183] = "nil", -- Blox Fruit Sea 2
+  [7449423635] = "nil", -- Blox Fruit Sea 3
+  [3623096087] = "nil", -- Muscle Legends
+  [8737899170] = "nil",  -- PS99 World 1
+  [16498369169] = "nil",  -- PS99 World 2
+  [17503543197] = "nil",  -- PS99 World 3
+  [10449761463] = "nil",  -- The Strongest Battlegrounds
+  [18901165922] = "nil", -- Pet GO
+  [16732694052] = "nil", -- Fisch
+  [116495829188952] = "nil", -- Dead Rails
+  [70876832253163] = "nil", -- Dead Rails
+}
+
+return Games
