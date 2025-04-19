5cee9a7ed90febf58cf4bc4130c5900ed12adc29
Nakhun12310 <163088668+Nakhun12310@users.noreply.github.com>
2025-04-19 17:12:06 +0700

    Update GrowAGarden.lua

--- GrowAGarden.lua
+++ GrowAGarden.lua
@@ -1,4 +1,3 @@
--- Load Fluent Renewed UI Library & Addons
 local Library = loadstring(game:HttpGetAsync("https://github.com/ActualMasterOogway/Fluent-Renewed/releases/latest/download/Fluent.luau"))()
 local SaveManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ActualMasterOogway/Fluent-Renewed/master/Addons/SaveManager.luau"))()
 local InterfaceManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ActualMasterOogway/Fluent-Renewed/master/Addons/InterfaceManager.luau"))()
